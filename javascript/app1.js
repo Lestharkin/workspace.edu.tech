be411642eeda4de68cf4b520af2a4fc1b0f5de46
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-03 20:23:01 -0500

    sync

---
+++ javascript/app1.js
@@ -0,0 +1,37 @@
+let num1 = 10
+let num2 = 20
+
+let res
+
+res = num1 + num2
+console.log(res)
+
+res = num1 - num2
+console.log(res)
+
+res = num1 * num2
+console.log(res)
+
+res = num1 / num2
+console.log(res)
+
+res = num1 % num2
+console.log(res)
+
+res = num1 ** num2
+console.log(res)
+
+console.log(++num1)
+
+console.log(num1++)
+console.log(num1)
+
+res = --num2
+console.log(res)
+
+res = num2--
+console.log(res)
+console.log(num2)
+
+res = 2 + 4 * 2 - 10 / 5 ** 2
+console.log(res)
