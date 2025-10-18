e274457dab85a2b7ca38b98e48c1f173c93fd662
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-17 21:28:38 -0500

    sync

--- javascript-ejercicios/ejercicio 2/index.js
+++ javascript-ejercicios/ejercicio 2/index.js
@@ -1,10 +1,35 @@
 const lista = document.getElementById("lista")
 const texto = document.getElementById("texto")
 const btnAgregar = document.getElementById("btnAgregar")
+const btnPromedio = document.getElementById("btnPromedio")
 
-function agregarElemento () {
-  const li = `<li class="list-group-item">${texto.value}</li>`
-  lista.innerHTML += li
-}
+const resultado = document.getElementById("resultado")
 
 btnAgregar.addEventListener("click", agregarElemento)
+
+
+function agregarElemento () {
+  const items = lista.childElementCount
+  if (items < 10) {
+    const li = `<li class="list-group-item">${texto.value}</li>`
+    lista.innerHTML += li
+  }
+  texto.value = ""
+  console.log(lista.children)
+
+  for (let i = 0; i < 7; i++) {
+    console.log(i)
+  }
+
+}
+
+btnPromedio.addEventListener("click", calcularPromedio)
+
+function calcularPromedio () {
+  let suma = 0
+  const items = lista.children
+  for (let i = 0; i < items.length; i++) {
+    suma += Number(items[i].innerText)
+  }
+  resultado.innerText = suma / items.length
+}
