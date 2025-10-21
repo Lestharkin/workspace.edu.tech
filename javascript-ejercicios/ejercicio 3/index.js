41c6fd9363ef12d92506181eff3e1e381b52d986
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-20 19:42:25 -0500

    sync

---
+++ javascript-ejercicios/ejercicio 3/index.js
@@ -0,0 +1,19 @@
+const inp = document.querySelector('#in')
+const btn = document.querySelector('#btn')
+const out = document.querySelector('#out')
+
+function pares (num) {
+  const resultados = []
+  for (let i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+      resultados.push(i)
+    }
+  }
+  return resultados
+}
+
+btn.addEventListener('click', () => {
+  const val = inp.value
+  const res = pares(Number(val))
+  out.innerText = res
+})
