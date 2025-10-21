c1867842c8c5e4ea3cdd09fa8e0b492a7805e8b2
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-20 20:44:57 -0500

    sync

--- javascript-ejercicios/ejercicio 3/index.js
+++ javascript-ejercicios/ejercicio 3/index.js
@@ -1,19 +1,39 @@
 const inp = document.querySelector('#in')
 const btn = document.querySelector('#btn')
 const out = document.querySelector('#out')
+const tbody = document.querySelector('#tbody')
 
-function pares (num) {
-  const resultados = []
-  for (let i = 0; i <= num; i++) {
-    if (i % 2 === 0) {
-      resultados.push(i)
-    }
-  }
-  return resultados
+// function pares (num) {
+//   const resultados = []
+//   for (let i = 0; i <= num; i++) {
+//     if (i % 2 === 0) {
+//       resultados.push(i)
+//     }
+//   }
+//   return resultados
+// }
+
+// btn.addEventListener('click', function () {
+//   const val = inp.value
+//   const res = pares(Number(val))
+//   out.innerText = res
+// })
+
+function pintarFilaTablaMultiplicar (num, multiplicador) {
+  const resultado = num * multiplicador
+  return `<tr>
+            <td>${num}</td>
+            <td>X</td>
+            <td>${multiplicador}</td>
+            <td>=</td>
+            <td>${resultado}</td>
+          </tr>`
 }
 
 btn.addEventListener('click', function () {
   const val = inp.value
-  const res = pares(Number(val))
-  out.innerText = res
+  tbody.innerHTML = ''
+  for (let i = 1; i <= 10; i++) {
+    tbody.innerHTML += pintarFilaTablaMultiplicar(Number(val), i)
+  }
 })
