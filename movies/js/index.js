e1cc31a80d53dc1ce70c759d9198b66d664a9014
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-24 21:08:13 -0500

    sync

--- movies/js/index.js
+++ movies/js/index.js
@@ -1,6 +1,6 @@
 const movies = document.querySelector('movies')
 
-const movieTemplate = movie => `
+const movieTemplate = async (movie) => `
 <div class="col-sm-12">
   <h2>Star Wars: ${movie.title}</h2>
   <h5>Fecha de estreno: ${movie.release_date}</h5>
@@ -11,23 +11,26 @@ const movieTemplate = movie => `
   </ul>
   <h3>Personajes</h3>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">Luke Skywalker</li>
-    <li class="list-group-item">Princess Leia</li>
-    <li class="list-group-item">Han Solo</li>
-    <li class="list-group-item">Darth Vader</li>
-    <li class="list-group-item">Obi-Wan Kenobi</li>
-    <li class="list-group-item">R2-D2</li>
-    <li class="list-group-item">C-3PO</li>
+  ${await charactersTemplate(movie.characters)}
   </ul>
 </div>`
+
+const charactersTemplate = async (characters) => {
+  let listItems = ''
+  for (const character of characters) {
+    const characterName = await getCharacterName(character)
+    listItems += `<li class="list-group-item">${characterName}</li>`
+  }
+  return listItems
+}
 
 const getMovies = () => {
   const response = fetch('https://swapi.info/api/films')
   response.then(res => {
     const resultado = res.json()
-    resultado.then(data => {
+    resultado.then(async data => {
       for (const movie of data) {
-        movies.innerHTML += movieTemplate(movie)
+        movies.innerHTML += await movieTemplate(movie)
       }
     }).catch(err => {
       console.log(err)
