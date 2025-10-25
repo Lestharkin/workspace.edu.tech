0ff5d0374b0c5fe3b130517eeca94c68a6d6d801
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-24 20:25:37 -0500

    sync

--- movies/js/index.js
+++ movies/js/index.js
@@ -1,3 +1,26 @@
+const movie = document.querySelector('movie')
+
+const movieTemplate = movie => `
+<div class="col-sm-12">
+  <h2>Star Wars: ${movie.title}</h2>
+  <h5>Fecha de estreno: ${movie.release_date}</h5>
+  <p>${movie.opening_crawl}</p>
+  <ul>
+    <li>Director: ${movie.director}</li>
+    <li>Productor: ${movie.producer}</li>
+  </ul>
+  <h3>Personajes</h3>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Luke Skywalker</li>
+    <li class="list-group-item">Princess Leia</li>
+    <li class="list-group-item">Han Solo</li>
+    <li class="list-group-item">Darth Vader</li>
+    <li class="list-group-item">Obi-Wan Kenobi</li>
+    <li class="list-group-item">R2-D2</li>
+    <li class="list-group-item">C-3PO</li>
+  </ul>
+</div>`
+
 const getMovies = () => {
   const response = fetch('https://swapi.info/api/films')
 
