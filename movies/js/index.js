a9b5436fa0b6bd3803dd4ec7d73d8bfe94b799ad
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-31 18:38:23 -0500

    sync

--- movies/js/index.js
+++ movies/js/index.js
@@ -1,14 +1,17 @@
 import { initStorage, getStoredMovies, prevIndex, nextIndex, getIndex } from './movie-storage.js'
 import { getMovieTemplate, paginationBtnTemplate } from './movie-template.js'
 
-(async () => {
-  await initStorage()
-  await render()
-})()
-
 const render = async () => {
   const movies = getStoredMovies()
   const moviesHTML = document.querySelector('movies')
   moviesHTML.innerHTML = await getMovieTemplate(movies[getIndex()])
   moviesHTML.appendChild(paginationBtnTemplate(prevIndex, nextIndex, render))
+}
+
+try {
+  await initStorage()
+  await render()
+} catch (error) {
+  console.error('Error initializing app:', error)
+  process.exit(1)
 }
