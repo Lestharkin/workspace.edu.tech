47764f4f09bc098ecfd334c0e54a1b5d0c2f55e9
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-11-07 19:00:07 -0500

    sync

---
+++ python/p001_print.py
@@ -0,0 +1,25 @@
+print("Hello, World!")
+print('Hello, "World"!')
+print("""Hello, 
+      Hello
+      Hello
+      Hello
+      World!""")
+print('''Hello, World!''')
+
+print("uno", "dos", "tres", 2)
+print("uno", "dos", "tres", 2, sep="-")
+
+print("""Hello, 
+      Hello
+      Hello
+      Hello
+      World!""" , end="\n")
+
+# Esto es un comentario
+"""Esto es un docstring
+
+d
+
+
+e varias líneas"""
